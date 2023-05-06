1aa00e3ff307122149755c0c82484255b0ba3212
Siddheswar Mukherjee <smukherjee@omp.com>
2023-05-06 17:00:56 +0200

    Add .stackdump to cleanup logic

--- __file_cleanup.bat
+++ __file_cleanup.bat
@@ -1,11 +1,18 @@
 @ECHO OFF
 
-ECHO Execting file cleanup...
+ECHO Executing file cleanup...
 ECHO Recursively deleting all *.exe file in the current directory.
 
 @ECHO OFF
 powershell -Command "& {ls *.exe -Recurse | foreach{echo $_}}"
 powershell -Command "& {ls *.exe -Recurse | foreach{rm $_}}"
+
+
+ECHO Recursively deleting all *.stackdump file in the current directory.
+
+@ECHO OFF
+powershell -Command "& {ls *.stackdump -Recurse | foreach{echo $_}}"
+powershell -Command "& {ls *.stackdump -Recurse | foreach{rm $_}}"
 
 
 PAUSE
