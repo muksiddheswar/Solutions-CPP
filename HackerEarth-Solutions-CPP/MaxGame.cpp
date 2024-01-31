ba761e4a3fc065dbc5e82763017cd44dbb6158b4
konkeror <sid.mkjee@gmail.com>
2024-01-31 23:21:46 +0100

    Merged repos

---
+++ HackerEarth-Solutions-CPP/MaxGame.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int solve (int N) {
+   // Write your code here
+   
+}
+
+int main() {
+
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int T;
+    cin >> T;
+    for(int t_i = 0; t_i < T; t_i++)
+    {
+        int N;
+        cin >> N;
+
+        int out_;
+        out_ = solve(N);
+        cout << out_;
+        cout << "\n";
+    }
+}
